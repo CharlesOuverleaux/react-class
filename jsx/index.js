e630ad2538c1cdfaa3d0abde5214ec3d80e4c90b
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-07-28 21:54:58 +0200

    Adding a function in jsx

--- jsx/index.js
+++ jsx/index.js
@@ -1,11 +1,17 @@
 // we define a class with the component name
+function greet() {
+  const greetings = ["Hi, ", "Hello! ", "Hi There! ", "Howdy mate! "];
+  const name = "Charles";
+  return greetings[Math.floor(Math.random() * 4)] + name;
+}
+
 class Hello extends React.Component {
   render() {
     // we need to add the () if there is more than one line
     // we can only return one element, so we need to wrap into a div
     return (
       <div>
-        <h1>Hello there, what's 2 * 2 ? It's {2*2}!</h1>
+        <h1>{greet()}</h1>
       </div>
     );
   }
