3d409400e26155e0ccad178703fe27b5b4b77a82
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-07-31 14:28:21 +0200

    Refactor

--- my_app/src/helpers.js
+++ my_app/src/helpers.js
@@ -1,10 +1,13 @@
 // choice(items): returns a randomly selected item from array of items
-function choice(items) {
-  return items[Math.floor(Math.random() * items.length)];
-}
+// function choice(items) {
+//   return items[Math.floor(Math.random() * items.length)];
+// }
+// we can refactor in a 1 liner, w. the new syntax for functions
+
+const choice = items => items[Math.floor(Math.random() * items.length)];
 
 // remove(items, item): removes the first matching item from items, if item exists, and returns it.Otherwise returns undefined.
-function remove(items, item) {
+const remove = (items, item) => {
   const index = items.indexOf(item);
   if (index > -1) {
     items.splice(index, 1);
