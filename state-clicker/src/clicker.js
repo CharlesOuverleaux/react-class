f8988d01619bafe40e928e7c07c739a48c9ad654
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-08-04 12:34:19 +0200

    css and correction

--- state-clicker/src/clicker.js
+++ state-clicker/src/clicker.js
@@ -4,16 +4,16 @@ import "./clicker.css"
 class Clicker extends Component {
   constructor(props){
     super(props);
-    this.state = { count: 0};
+    this.state = { count: 1};
     this.handleClick = this.handleClick.bind(this);
   };
   handleClick () {
-    let rand = Math.floor(Math.random() * 10);
+    let rand = Math.floor(Math.random() * 10) + 1;
     this.setState({ count: rand});
   };
   render () {
     const btn = <button onClick={this.handleClick}>Click here for a new number</button>;
-    const winMsg = <h2>You won!</h2>
+    const winMsg = <h2 className="win">You won!</h2>
     let win = this.state.count === 7;
     return (
       <div className="Clicker">
