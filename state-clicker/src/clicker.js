68bc04ecc4bc6912588eb58f9c68761076f78aa0
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-08-04 12:10:03 +0200

    Added handle click to get rnd number

--- state-clicker/src/clicker.js
+++ state-clicker/src/clicker.js
@@ -5,11 +5,17 @@ class Clicker extends Component {
   constructor(props){
     super(props);
     this.state = { count: 0};
+    this.handleClick = this.handleClick.bind(this);
+  };
+  handleClick () {
+    let rand = Math.floor(Math.random() * 10);
+    this.setState({ count: rand});
   };
   render () {
     return (
       <div className="Clicker">
         <h1>Your number is: {this.state.count}</h1>
+        <button onClick={this.handleClick}>Click here for a new number</button>
       </div>
     );
   }
