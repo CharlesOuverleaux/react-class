5336c3b348c9d928f19b9d71419f17c62f221b85
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-08-04 15:42:33 +0200

    Added component wi rollDice

--- roll-dice/src/rollDice.js
+++ roll-dice/src/rollDice.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import Die from "./die";
 
 class RollDice extends Component {
   render() {
     return (
       <div className="rollDice">
         <h1>Welcome to a Dice roll</h1>
+        <Die n="four" />
       </div>
     );
   }
