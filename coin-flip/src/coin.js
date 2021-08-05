b2ea771c26916e047496b5fedcf7ae4de55cd59e
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-08-05 16:04:31 +0200

    Refactor state logic

--- coin-flip/src/coin.js
+++ coin-flip/src/coin.js
@@ -3,11 +3,9 @@ import "./coin.css";
 
 class Coin extends Component {
   render() {
-    const head = <img alt="head" src="https://images.unsplash.com/photo-1588866054175-c8347662ec72?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80" />
-    const tail = <img alt="tail" src="https://images.unsplash.com/photo-1510920018318-3b4dfe979e4d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80" />
     return(
       <div className="Coin">
-        { this.props.flipResult ? head : tail }
+        <img alt={this.props.info.side} src={this.props.info.imgSrc } />
       </div>
     );
   }
--- coin-flip/src/flip.js
+++ coin-flip/src/flip.js
@@ -3,20 +3,25 @@ import Coin from './coin';
 import './flip.css';
 
 class Flip extends Component {
+  static defaultProps = {
+    coins: [
+      { side: "heads", imgSrc:"https://images.unsplash.com/photo-1588866054175-c8347662ec72?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80"},
+      { side: "tails", imgSrc: "https://images.unsplash.com/photo-1510920018318-3b4dfe979e4d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80"}
+    ]
+  }
   constructor(props){
     super(props);
-    this.state = { flip: true, counter: 0, heads: 0, tails: 0}
+    this.state = { currCoin: null, flip: true, counter: 0, heads: 0, tails: 0}
     this.handleClick = this.handleClick.bind(this)
   }
   getrndflip(){
-    const arr = [true, false]
-    const choice = arr[(Math.floor(Math.random() * 2))]
+    const newCoin = this.props.coins[(Math.floor(Math.random() * 2))]
     this.setState(st => {
       return {
-        flip: choice,
+        currCoin: newCoin,
         counter: st.counter + 1,
-        heads: st.heads + (choice ? 1 : 0),
-        tails: st.tails + (choice ? 0 : 1)
+        heads: st.heads + (newCoin.side === "heads" ? 1 : 0),
+        tails: st.tails + (newCoin.side === "tails" ? 1 : 0)
       }
     })
   }
@@ -27,7 +32,7 @@ class Flip extends Component {
   render() {
     return(
       <div className="Flip">
-        <Coin flipResult={this.state.flip} />
+        {this.state.currCoin && <Coin info={this.state.currCoin}/>}
         <button onClick={this.handleClick}>Flip me!</button>
         <p>Played {this.state.counter} times. Heads: {this.state.heads} vs Tails: {this.state.tails}</p>
       </div>
