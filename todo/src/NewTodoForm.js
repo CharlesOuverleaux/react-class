3c3ae3149c647772d7f6e51dc6783b44f995edf4
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-08-16 12:14:46 +0200

    Added task form

--- todo/src/NewTodoForm.js
+++ todo/src/NewTodoForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './NewTodoForm.css';
+import { v4 as uuidv4 } from 'uuid';
 
 class NewTodoForm extends Component {
   constructor(props){
@@ -7,14 +8,33 @@ class NewTodoForm extends Component {
     this.state = {
       taskName: ''
     }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const newTask = { ...this.state, id: uuidv4() }
+    this.props.createTask(newTask);
+  }
+
   render() {
     return(
       <div className='NewTodoForm'>
-        <p>I'm a form</p>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label htmlFor='taskName'></label>
-          <input name='taskName' value={this.state.taskName}></input>
+          <input
+            name='taskName'
+            value={this.state.taskName}
+            onChange={this.handleChange}
+            placeholder="Add a new task...">
+            </input>
           <button>Add</button>
         </form>
       </div>
