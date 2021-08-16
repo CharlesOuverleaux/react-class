cfbaa1b1babdd6696b2fe5c2c3cb745ff2b082c5
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-08-16 08:40:01 +0200

    Added Todo component

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Todo from './Todo';
 
 function App() {
   return (
     <div className="App">
       <h1>Todo App</h1>
+      <Todo />
     </div>
   );
 }
---
+++ todo/src/Todo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Todo.css';
+
+
+class Todo extends Component {
+  render() {
+    return(
+      <div className="Todo">
+        <p>Todo taks tite</p>
+      </div>
+    )
+  }
+}
+
+export default Todo;
