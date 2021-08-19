e13d0353c9fda51f3c9c83e40a97057c7a2c8614
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-08-19 11:58:23 +0200

    Completed rules logic

--- yahztee/yahtzee-starter/src/Die.js
+++ yahztee/yahtzee-starter/src/Die.js
@@ -2,12 +2,23 @@ import React, { Component } from "react";
 import "./Die.css";
 
 class Die extends Component {
+  constructor(props){
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.props.handleClick(this.props.idx);
+  }
   render() {
     return (
       <button
         className={"Die"}
         style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={this.props.handleClick}
+        // we pass the whole event, but here we only want the handleClick
+        // we can do this w. an arrow function, onClick={() => this.props.handleClick(this.props.idx)}
+        // but that's not optimal for perf, so we do a new function
+        // whenever we do a function we need to bind this, otherwise this is undefeined
+        onClick={this.handleClick}
       >
         {this.props.val}
       </button>
