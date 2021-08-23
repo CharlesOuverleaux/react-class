3da766506056553a348ae1daa686baa3a0c4c154
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-08-23 10:17:27 +0100

    Added descriptions of each play

--- yahztee/yahtzee-starter/src/RuleRow.js
+++ yahztee/yahtzee-starter/src/RuleRow.js
@@ -3,13 +3,13 @@ import './RuleRow.css'
 
 class RuleRow extends Component {
   render() {
-    const {doScore, name , score} = this.props;
+    const {doScore, name , score, description} = this.props;
     let activated = score === undefined;
     return (
       <tr className={`RuleRow RuleRow-${activated ? 'active' : 'disabled'}`}
           onClick={activated ? doScore : null }>
         <td className="RuleRow-name">{name}</td>
-        <td className="RuleRow-score">{score}</td>
+        <td className="RuleRow-score">{activated ? description : score}</td>
       </tr>
     )
   }
