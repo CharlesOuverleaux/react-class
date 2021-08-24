c423a2ee7574a957c9be91039278acc2f23bc6f4
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-08-24 12:01:10 +0100

    Created deck of cards app

---
+++ deck-of-cards/deck-of-cards/src/Deck.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import './Deck.css';
+
+class Deck extends Component {
+  render() {
+    return (
+      <div className='Deck'>
+        <h1>The deck of cards</h1>
+      </div>
+    );
+  }
+}
+
+export default Deck;
