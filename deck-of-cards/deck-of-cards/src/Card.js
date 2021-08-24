d6d7b1091bf05bd2efc5599e30635810d296e90d
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-08-24 18:23:48 +0100

    Added card image & deck effect

--- deck-of-cards/deck-of-cards/src/Card.js
+++ deck-of-cards/deck-of-cards/src/Card.js
@@ -3,12 +3,16 @@ import './Card.css';
 
 
 class Card extends Component {
-
+  constructor(props) {
+    super(props);
+    let angle = Math.random() * 90 - 45;
+    let xPos = Math.random() * 40 - 20;
+    let yPos = Math.random() * 40 - 20;
+    this._transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`;
+  }
   render () {
     return (
-      <div className='Card'>
-        <img alt={this.props.cardInfo.code} src={this.props.cardInfo.images.svg} />
-      </div>
+      <img className='Card' style={{ transform: this._transform }} alt={this.props.cardInfo.code} src={this.props.cardInfo.image} />
     )
   }
 }
--- deck-of-cards/deck-of-cards/src/Deck.js
+++ deck-of-cards/deck-of-cards/src/Deck.js
@@ -7,25 +7,28 @@ const API_BASE_URL = 'https://deckofcardsapi.com/api/deck/'
 class Deck extends Component {
   constructor(props){
     super(props);
-    this.state = {deck_id: null, cards:[]}
+    this.state = {deck_id: null, cards:[], remaining: null}
     this.getCard = this.getCard.bind(this);
   }
   async componentDidMount(){
     let deck = await axios.get(API_BASE_URL + 'new/shuffle/');
-    this.setState({deck_id: deck.data.deck_id})
+    this.setState({deck_id: deck.data.deck_id , remaining: deck.data.remaining})
   }
   async getCard(){
     let deck_id =  this.state.deck_id;
     let card = await axios.get(API_BASE_URL + `${deck_id}/draw/`);
-    this.setState({ cards: [...this.state.cards, card.data.cards[0]]});
+    this.setState({ cards: [...this.state.cards, card.data.cards[0]],remaining: this.state.remaining - 1});
   }
   render() {
     let cards = this.state.cards.map(card => <Card key= {card.code} cardInfo={card}/>)
+    let end_game = this.state.remaining === 0;
     return (
       <div className='Deck'>
-        <h1>The deck of cards</h1>
-        <button onClick={this.getCard}>Get a card!</button>
+        <h1>The deck of cards: {this.state.remaining}/52</h1>
+        {end_game ? <p>You have used all cards</p> : <button onClick={this.getCard}>Get a card!</button>}
+        <div className='Card-area'>
         {cards}
+        </div>
       </div>
     );
   }
