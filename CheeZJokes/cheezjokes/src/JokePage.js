c22619e4a87903b2951ec7a4c3148a74cd34b5a4
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-08-25 16:36:55 +0100

    Added jokes local storage

--- CheeZJokes/cheezjokes/src/JokePage.js
+++ CheeZJokes/cheezjokes/src/JokePage.js
@@ -7,19 +7,22 @@ import uuid from 'uuid/v4';
 class JokePage extends Component {
   constructor(props){
     super(props);
-    this.state = { jokes: []}
+    this.state = { jokes: JSON.parse(window.localStorage.getItem('jokes')) || '[]'};
   }
-  async componentDidMount(){
+  componentDidMount(){
+    if(this.state.jokes.length ===0) this.getJokes();
+  }
+  async getJokes(){
     let jokes = [];
     for (let i = 0; i < 10; i++) {
       let res = await axios.get('https://icanhazdadjoke.com/', { headers: { Accept: 'Application/json' } });
-      jokes.push({joke: res.data.joke, votes: 0, id: uuid()});
+      jokes.push({ joke: res.data.joke, votes: 0, id: uuid() });
     }
     this.setState({
       jokes: jokes
     })
+    window.localStorage.setItem("jokes", JSON.stringify(jokes));
   }
-
   handleVote(id, delta){
     this.setState(st => ({
       jokes: st.jokes.map(j =>
