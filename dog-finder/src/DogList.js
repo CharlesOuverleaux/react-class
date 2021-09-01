bb2eb4f124a5d7da61b85b69a11999877f786a06
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-09-01 10:51:58 +0100

    Added DogList component

---
+++ dog-finder/src/DogList.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import './DogList.css';
+
+class DogList extends Component {
+  render(){
+    return(
+      <div className='DogList'>
+      <h1>I'm a DogList</h1>
+      </div>
+    )
+  }
+}
+
+export default DogList;
