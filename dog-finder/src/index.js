7ffc1eeb6ad2813733c2a8449c2b75a2fa98505b
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-09-01 10:44:19 +0100

    Added navbar bs

--- dog-finder/src/index.js
+++ dog-finder/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// add both to make bootstrap work
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
