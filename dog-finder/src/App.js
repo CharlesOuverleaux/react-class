6159a14c988ba29258d728aa34804c5b7f80c05a
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-09-01 10:32:13 +0100

    Fixed bootstrap import bug

--- dog-finder/src/App.js
+++ dog-finder/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Dog Finder App</h1>
+      <h1 class="alert alert-primary">Dog Finder App</h1>
     </div>
   );
 }
