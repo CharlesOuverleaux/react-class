6d6b3511b201422655152b0ddab602407716366d
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-09-03 15:30:51 +0100

    Added CNJ empty component

---
+++ Interviews/api-call/src/ChuckNorrisJokes.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./ChuckNorrisJokes.css";
+
+class ChuckNorrisJokes extends Component {
+  render(){
+    return(
+      <div className='ChuckNorrisJokes'>
+        <h1>I'm a joke!</h1>
+      </div>
+    )
+  }
+}
+
+export default ChuckNorrisJokes;
