ab1f867e609c8a955ad40675b56d4df7c673b0c1
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-09-06 14:33:47 +0100

    API init

---
+++ Interviews/api-call-2/src/Movie.js
@@ -0,0 +1,10 @@
+import React from "react";
+import axios from "axios";
+
+export default function Movie(){
+  return(
+    <div className="Movie">
+      <h1>Star Wars Movies</h1>
+    </div>
+  );
+}
