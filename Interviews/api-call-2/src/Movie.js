fab74ec20bf48d3b6bb9d5e3f7821b5c53bac69c
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-09-06 15:27:27 +0100

    Added movie description

--- Interviews/api-call-2/src/Movie.js
+++ Interviews/api-call-2/src/Movie.js
@@ -1,22 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function Movie(){
+  const [number, setNumber] = useState(1);
   const [movie, setMovie] = useState("") ;
+  const movieNumbers = [1,2,3,4,5,6];
+  const [people, setPeople] = useState(0);
 
-  async function getMovie() {
-    try {
-      const response = await axios.get('https://swapi.dev/api/films/1');
-      setMovie(response.data);
-    } catch (error) {
-      console.error(error);
+  useEffect(() => {
+    async function getMovie() {
+      try {
+        const response = await axios.get(`https://swapi.dev/api/films/${number}`);
+        setMovie(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
     }
-  }
+    getMovie();
+    // we add in number as a second argument,
+    // this way it runs only when number is updated
+    // otherwise endless loop bc it runs after each render
+  },[number]);
+
   return(
     <div className="Movie">
-      <h1>Star Wars Movies</h1>
-      <button onClick={getMovie}>Get Movie</button>
-      <p>{movie.title}</p>
+      <h1>Star Wars Movies: {movie.title}</h1>
+      <select value={number} onChange={e => setNumber(e.target.value)}>
+        {movieNumbers.map(n => <option key={n} value={n}>{n}</option>)}
+      </select>
+      <p>{movie.opening_crawl}</p>
+      <ul>
+
+      </ul>
     </div>
   );
 }
