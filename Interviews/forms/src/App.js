081bd035bb10517178c3bb16e674961180c45c6a
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-09-06 16:14:15 +0100

    Added todo component

--- Interviews/forms/src/App.js
+++ Interviews/forms/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import ToDoApp from './ToDoApp';
 
 function App() {
   return (
     <div className="App">
+      <ToDoApp />
     </div>
   );
 }
---
+++ Interviews/forms/src/ToDoApp.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./ToDoApp.css";
+
+export default function ToDoApp (){
+  return(
+    <div className='ToDoApp'>
+      <h1>To Do's</h1>
+    </div>
+  );
+}
