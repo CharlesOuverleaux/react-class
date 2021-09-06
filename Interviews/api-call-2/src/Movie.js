7a48611aacf835d60189b5dcadfb96f9db75f83d
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-09-06 14:46:40 +0100

    Added Movie component, and API to get movie title

--- Interviews/api-call-2/src/Movie.js
+++ Interviews/api-call-2/src/Movie.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 export default function Movie(){
+  const [movie, setMovie] = useState("") ;
+
+  async function getMovie() {
+    try {
+      const response = await axios.get('https://swapi.dev/api/films/1');
+      setMovie(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
   return(
     <div className="Movie">
       <h1>Star Wars Movies</h1>
+      <button onClick={getMovie}>Get Movie</button>
+      <p>{movie.title}</p>
     </div>
   );
 }
