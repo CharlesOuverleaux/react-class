08d728424db241ce0bc3e40b139069c24854b589
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-09-06 17:39:20 +0100

    Added todolist component

--- Interviews/forms/src/ToDoApp.js
+++ Interviews/forms/src/ToDoApp.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState }from "react";
 import "./ToDoApp.css";
 import { Typography, Paper, AppBar, Toolbar, Grid } from '@material-ui/core';
+import ToDoList from './ToDoList';
 
 export default function ToDoApp (){
+  const initialToDos = [
+    { id: 1, task:"wash bedsheets", complete: false},
+    { id: 2, task: "clean room", complete: false },
+    { id: 3, task: "go shopping", complete: false }
+  ]
+  const [todos, setTodos] = useState(initialToDos);
   return(
     <Paper
       style={{
@@ -12,12 +19,15 @@ export default function ToDoApp (){
         backgroundColor: '#fafafa'
       }}
       elevation={0}
-    >
+      >
       <AppBar>
-        <Toolbar color='primary' position='static' style={{height:'64px'}}>
-          <Typography>TO DO LIST</Typography>
+        <Toolbar color='primary' position='static' style={{ height:'64px'}}>
+          <Typography color='inherit'>TO DO LIST</Typography>
         </Toolbar>
       </AppBar>
+      <div className='Spacer'>
+        <ToDoList todos={initialToDos} />
+      </div>
     </Paper>
   );
 }
---
+++ Interviews/forms/src/ToDoList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Paper, List, ListItem, Divider } from "@material-ui/core";
+
+export default function ToDoList(props) {
+  return(
+    <Paper>
+      <List>
+        {props.todos.map(td => <ListItem>{td.task}</ListItem>)}
+      </List>
+    </Paper>
+  );
+}
