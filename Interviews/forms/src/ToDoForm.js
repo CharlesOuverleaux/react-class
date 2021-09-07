031bebd188f38529f49305fb23ed8404fa9284f0
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-09-07 10:47:17 +0100

    Hooked form

--- Interviews/forms/src/ToDoForm.js
+++ Interviews/forms/src/ToDoForm.js
@@ -1,12 +1,19 @@
-import { TextField } from "@material-ui/core";
+import { Paper, TextField } from "@material-ui/core";
 import React from "react";
+import useInputState from "./hooks/useInputState";
 import './ToDoForm.css';
 
 
 export default function ToDoForm(props){
+  const [value , handleChange, reset] = useInputState('');
   return(
     <div className='ToDoForm'>
-      <TextField />
+      <Paper>
+        <form>
+        <TextField value={value} onChange={handleChange}/>
+        <button onClick={reset}>Submit</button>
+        </form>
+      </Paper>
     </div>
   );
 }
