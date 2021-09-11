6a16514f62a7185a81bea7d311fddda09d7649b5
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-09-11 13:57:33 +0100

    API needs 10 for approval

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import TwitterDash from './TwitterDash';
 
 function App() {
   return (
     <div className="App">
-
+      <TwitterDash />
     </div>
   );
 }
---
+++ dashboard/src/keys.js
@@ -0,0 +1,4 @@
+const API_key = 'z21fmCXAzGLj5jcSvLdyRKqTR';
+const API_key_secret = 'T2UfjM0XV5KJ6hDCIj2aF9DT5oNmC7kpFLkBEQc5SpHBBgcXc4';
+const Bearer_Token = 'AAAAAAAAAAAAAAAAAAAAAIlgTgEAAAAAjGx96GKyOAzoXMqQQczBpP9FadU%3DyjOIxtxk9XPVZu2jwWmm9fu4m3IUxQxAXCe8n3LDPt4Yci4KhY';
+const app_id = '21913737';
