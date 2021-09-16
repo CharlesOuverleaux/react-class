4ddb7004ecb4a5af342286c6eb8958116eb21cc8
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-09-16 17:09:57 +0100

    Added useReducer

--- todos-context/src/contexts/todos.context.js
+++ todos-context/src/contexts/todos.context.js
@@ -1,18 +1,18 @@
-import React, { createContext } from 'react';
-import useTodoState from '../hooks/useTodoState';
+import React, { createContext, useReducer } from 'react';
+import reducer from '../reducers/todos.reducer';
 
 const defaultTodos = [
-  { id: 1, task: "Walk The Goldfish", completed: true },
-  { id: 2, task: "Walk dogs", completed: false }
+  { id: 1, task: "Draw a unicorn", completed: true },
+  { id: 2, task: "Buy more club mate", completed: false }
 ];
 
 export const TodosContext = createContext();
 
 export function TodosProvider(props){
-  const todoData = useTodoState(defaultTodos);
+  const [todos, dispatch] = useReducer(reducer, defaultTodos )
   return (
     // value can only be a single item, we wrap all in context provider
-    <TodosContext.Provider value={todoData}>
+    <TodosContext.Provider value={{todos, dispatch}}>
     {/* all children will get the context */}
       {props.children}
     </TodosContext.Provider>
---
+++ todos-context/src/reducers/todos.reducer.js
@@ -0,0 +1,24 @@
+import uuid from "uuid/v4";
+
+const reducer = (state, action) => {
+  switch(action.type){
+    case "ADD":
+      return [...state, {id: uuid(), task: action.task, completed: false}];
+    case "REMOVE":
+      return state.filter(todo => todo.id !== action.id);
+    case "TOGGLE":
+      return state.map(todo => todo.id === action.id ? { ...todo, completed: !todo.completed } : todo);
+    case "EDIT":
+      return state.map(todo => todo.id === action.id ? { ...todo, task: action.task } : todo);
+    default:
+      return state;
+  }
+}
+
+export default reducer;
+
+// Todo cases
+// { type: "ADD", task: newTodoText }
+// { type: "REMOVE", id: todoId }
+// { type: "TOGGLE", id: todoId }
+// { type: "EDIT", id: todoId, task: newTask }
