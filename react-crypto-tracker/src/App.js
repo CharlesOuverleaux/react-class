49e0a0e9e35037418b1e46abb9c498e9b3b67f50
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-10-16 12:54:46 +0200

    Added coingecko api

--- react-crypto-tracker/src/App.js
+++ react-crypto-tracker/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false').then(res => {
+      setCoins(res.data)
+    }).catch(err => console.log(err))
+  }, []);
   return (
     <div className="App">
       <h1>Crypto-Tracker</h1>
+        <ul>
+          {coins.map(coin => <li>{coin.id}</li>)}
+        </ul>
     </div>
   );
 }
