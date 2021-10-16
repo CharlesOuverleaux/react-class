eaa37d656812d71b4b564e2705cc7ab56380b34d
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-10-16 16:40:02 +0200

    conditional colors for price changes

--- react-crypto-tracker/src/Coin.js
+++ react-crypto-tracker/src/Coin.js
@@ -2,15 +2,14 @@ import './Coin.css';
 
 function Coin(props) {
   const coin = props.coin;
+  const priceChange = coin.price_change_percentage_24h;
   return(
     <tr className="coin-row">
-      <td>
-        <img alt={coin.id} src={coin.image} />
-      </td>
+      <td> <img alt={coin.id} src={coin.image} /> </td>
       <td className='coin-name'>{coin.name}</td>
       <td className='coin-symbol'>{coin.symbol.toUpperCase()}</td>
       <td>€{coin.current_price.toLocaleString()}</td>
-      <td>{coin.price_change_percentage_24h}%</td>
+      <td style={ {color: priceChange < 0 ? 'red' : 'green'}}>{priceChange.toFixed(2)}%</td>
       <td>€{coin.ath.toLocaleString()}</td>
       <td>€{coin.market_cap.toLocaleString()}</td>
       <td>€{coin.total_volume.toLocaleString()}</td>
