c0f5c4e369cb878d9fba638f03e96b6c227dc157
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-10-16 12:34:37 +0200

    Init crypto-tracker

--- Next/test-apps/next-os/pages/index.js
+++ Next/test-apps/next-os/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from '../styles/Home.module.css';
 import axios from 'axios';
+import Image from 'next/image'
 
 export async function getStaticProps() {
-  const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+  const res = await axios.get('https://api.opensea.io/api/v1/assets?order_by=sale_count&order_direction=desc&offset=0&limit=20');
   const { data } = res;
+  console.log(data);
   return {
     props: {data}
   }
@@ -13,9 +15,12 @@ export async function getStaticProps() {
 export default function Home({data}) {
   return (
     <div className={styles.container}>
-      <h1>Hello</h1>
+      <h1>Top 20 NFT&apos;s by sales count</h1>
         <ol className='List'>
-        {data.map(i => <li key={i.id}>{i.title}</li>)}
+        {data.assets.map(i =>
+          <li key={i.id}>
+            {i.name} - {i.description}
+          </li>)}
         </ol>
     </div>
   )
