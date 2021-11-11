2e630993304f2e8a634764ccd4f2bd55e05781e0
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-11-11 16:48:27 +0100

    Added navigation names

--- react-crypto-tracker/src/Home.js
+++ react-crypto-tracker/src/Home.js
@@ -4,9 +4,11 @@ import { Link } from 'react-router-dom'
 export default function Home(){
   return(
     <div>
-      <h1>Welcome to NFT launcher</h1>
+      <h1>NFTigers</h1>
       <nav>
-        <Link to="/nfts">NFT's</Link>
+        <Link to="/nfts">Top NFT's</Link>
+        <Link to="/nfts/launches">Newest Collections</Link>
+        <Link to="/nfts/launches/add">List your project</Link>
         <Link to="/coins-dashboard">Crypto Dashboard</Link>
       </nav>
     </div>
