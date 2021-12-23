0c3331b1a20cc34e169ce0a76eb4a70875403c3d
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-12-23 10:56:21 +0100

    Added all events page data

--- nextjs-course-router-project/components/layout/main-header.js
+++ nextjs-course-router-project/components/layout/main-header.js
@@ -4,12 +4,12 @@ export default function MainHeader() {
   return (
     <header className="flex justify-around text-white mb-4 bg-blue-500 p-4">
       <div>
-        <Link href="/">Events</Link>
+        <Link href="/">Featured Events</Link>
       </div>
       <nav>
         <ul>
           <li>
-            <Link href="/events">Browse all events</Link>
+            <Link href="/events">All events</Link>
           </li>
         </ul>
       </nav>
--- nextjs-course-router-project/pages/events/index.js
+++ nextjs-course-router-project/pages/events/index.js
@@ -1,7 +1,13 @@
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/EventList";
+
 export default function EventsPage() {
+  const events = getAllEvents();
+
   return (
     <div>
-      <h1>EventsPage</h1>
+      <h1>All events</h1>
+      <EventList items={events} />
     </div>
   );
 }
