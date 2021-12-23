78656d4e7392215f81d904e4f6f3c522a14988a8
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-12-23 11:15:37 +0100

    Added search bar

--- nextjs-course-router-project/components/events/EventSearch.js
+++ nextjs-course-router-project/components/events/EventSearch.js
@@ -1,14 +1,14 @@
 export default function EventSearch(props){
   return (
     <form className="flex justify-center rounded-lg bg-white p-2">
-      <div>
+      <div className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
         <label htmlFor="year">Year</label>
         <select id="year">
           <option value="2021">2021</option>
           <option value="2022">2022</option>
         </select>
       </div>
-      <div>
+      <div className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
         <label htmlFor="month">Month</label>
         <select id="month">
           <option value="1">January</option>
@@ -25,7 +25,9 @@ export default function EventSearch(props){
           <option value="12">December</option>
         </select>
       </div>
-      <button>Search events</button>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white p-2 font-bold rounded-lg">
+        Search
+      </button>
     </form>
   );
 }
