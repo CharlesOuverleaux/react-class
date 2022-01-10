9491d26e4cdd58addc8cc8ab1e76fdaeea736e36
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2022-01-10 15:57:00 +0000

    Added standard meta data for all pages

--- nextjs-course-router-project/pages/_app.js
+++ nextjs-course-router-project/pages/_app.js
@@ -1,9 +1,14 @@
 import Layout from '../components/layout/layout'
 import '../styles/globals.css'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>NFT events</title>
+        <meta name="description" content="Find the best NFT events in your city" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
