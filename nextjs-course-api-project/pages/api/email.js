1eea9c38cd4a129ecff8097843ee3852e508716e
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2022-01-13 18:28:16 +0000

    Added MongoDb

--- nextjs-course-api-project/pages/api/email.js
+++ nextjs-course-api-project/pages/api/email.js
@@ -1,20 +1,20 @@
-import fs from 'fs';
-import path from 'path';
+import { MongoClient } from "mongodb";
 
-export default function handler(request, response){
-  if(request.method === 'POST'){
-    const email = request.body.email;
-    const newEmail = {
-      id: new Date().toISOString(),
-      email: email,
-    };
-    const filePath = path.join(process.cwd(), "data", "email.json");
-    const fileData = fs.readFileSync(filePath);
-    const data = JSON.parse(fileData);
-    data.push(newEmail);
-    fs.writeFileSync(filePath, JSON.stringify(data));
-    response.status(201).json({message: 'success', email: newEmail });
+export default async function handler(request, response) {
+  if (request.method === "POST") {
+    const userEmail = request.body.email;
+    const client = await MongoClient.connect(
+      "mongodb+srv://charly:TestCharly@cluster0.cesdy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+    await db.collection("emails").insertOne({ email: userEmail });
+
+    client.close();
+
+    response.status(201).json({ message: "success", email: userEmail });
   } else {
-    response.status(200).json({message: 'successful request but only put allowed so far'})
+    response
+      .status(200)
+      .json({ message: "successful request but only put allowed so far" });
   }
 }
