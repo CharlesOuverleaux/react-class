cb9d80dfac4a980f8167a3695ec96997156306e0
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2022-01-14 12:14:35 +0000

    Refactor: Added getAllDocuments. Added error handling for comments

--- nextjs-course-api-project/helpers/db-utils.js
+++ nextjs-course-api-project/helpers/db-utils.js
@@ -1,16 +1,25 @@
 import { MongoClient } from "mongodb";
 
-async function connectDatabase() {
+export async function connectDatabase() {
   const client = await MongoClient.connect(
     "mongodb+srv://charly:TestCharly@cluster0.cesdy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
   );
   return client;
 }
 
-async function insertDocument(client, collection, document) {
+export async function insertDocument(client, collection, document) {
   const db = client.db();
   const result = await db.collection(collection).insertOne(document);
   return result;
 }
 
-export { connectDatabase, insertDocument };
+export async function getAllDocuments(client, collection, sort) {
+  const db = client.db();
+  const documents = await db
+    .collection(collection)
+    .find()
+    // e.g. { _id: -1 }
+    .sort(sort)
+    .toArray();
+  return documents;
+}
--- nextjs-course-api-project/pages/api/comments/[eventId].js
+++ nextjs-course-api-project/pages/api/comments/[eventId].js
@@ -1,9 +1,20 @@
-import { connectDatabase, insertDocument } from "../../../helpers/db-utils";
+import {
+  connectDatabase,
+  insertDocument,
+  getAllDocuments,
+} from "../../../helpers/db-utils";
 
 export default async function handler(request, response) {
   const eventId = request.query.eventId;
+  let client;
 
-  const client = await connectDatabase();
+  try {
+    client = await connectDatabase();
+  } catch (error) {
+    response.status(500).json({ message: "Database connection failed" });
+    // in case of error we want to stop the code there
+    return;
+  }
 
   if (request.method === "POST") {
     const { email, name, text } = request.body;
@@ -13,21 +24,29 @@ export default async function handler(request, response) {
       text: text,
       eventId: eventId,
     };
-    const result = await insertDocument(client, "comments", newComment);
-    newComment.id = result.insertedId;
 
-    response
-      .status(201)
-      .json({ message: "Added comment", comment: newComment });
+    let result;
+    try {
+      result = await insertDocument(client, "comments", newComment);
+      newComment._id = result.insertedId;
+      response
+        .status(201)
+        .json({ message: "Added comment", comment: newComment });
+    } catch (error) {
+      response.status(500).json({ message: "Database insert failed" });
+    }
   }
   if (request.method === "GET") {
-    const db = client.db();
-    const documents = await db
-      .collection("comments")
-      .find()
-      .sort({ _id: -1 })
-      .toArray();
-    response.status(200).json({ comments: documents });
+    try {
+      const documents = await getAllDocuments(client, "comments", {
+        _id: -1,
+      });
+      response.status(200).json({ comments: documents });
+    } catch (error) {
+      response
+        .status(500)
+        .json({ message: "Database getting all documents failed" });
+    }
   }
   client.close();
 }
