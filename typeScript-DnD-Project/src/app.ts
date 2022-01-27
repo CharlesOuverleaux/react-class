f1f098f6c817d6856a78c81c714e4de96c0ffc7e
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2022-01-27 15:58:02 +0100

    Added form rendering

--- typeScript-DnD-Project/src/app.ts
+++ typeScript-DnD-Project/src/app.ts
@@ -1,1 +1,27 @@
-// Code goes here!
+
+class ProjectInput {
+  // define the types
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor(){
+    // select the elements
+    this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    // accessing the elements
+    const importedNode = document.importNode(this.templateElement.content, true);
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+
+    // excuting the rendering
+    this.attach();
+  }
+
+  // rendering
+  private attach(){
+    this.hostElement.insertAdjacentElement('afterbegin', this.element)
+  }
+}
+
+const prjInput = new ProjectInput();
